54d15d35f3f0b910af1e0525c8175b2b509a3c2f
Wei Heng <e1397981@u.nus.edu>
2026-01-24 12:39:39 +0800

    Level 0: Rename, Greet, Exit

---
+++ src/main/java/Codey.java
@@ -0,0 +1,15 @@
+public class Codey {
+    public static void main(String[] args) {
+        String logo =
+                "  ____          _           \n" +
+                        " / ___|___   __| | ___ _   _ \n" +
+                        "| |   / _ \\ / _` |/ _ \\ | | |\n" +
+                        "| |__| (_) | (_| |  __/ |_| |\n" +
+                        " \\____\\___/ \\__,_|\\___|\\__, |\n" +
+                        "                        |___/ \n";
+        System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+
+        System.out.println("Bye! Hope to see you again soon!");
+    }
+}
