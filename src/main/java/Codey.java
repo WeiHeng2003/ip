bd91ed821e37fead6a8cb6daa7dc284f40133cf0
Wei Heng <e1397981@u.nus.edu>
2026-01-25 19:47:36 +0800

    Revert "Changed codeStyles for switch case"
    This reverts commit e3790bd63b648f1481cf74ade2c106bb405a928a.

--- src/main/java/Codey.java
+++ src/main/java/Codey.java
@@ -1,6 +1,3 @@
-import java.util.Scanner;
-import java.util.Arrays;
-
 public class Codey {
     public static void main(String[] args) {
         String logo =
@@ -10,27 +7,9 @@ public class Codey {
                         "| |__| (_) | (_| |  __/ |_| |\n" +
                         " \\____\\___/ \\__,_|\\___|\\__, |\n" +
                         "                        |___/ \n";
-        System.out.println("_______________________________________________________");
-        System.out.println("Hello I'm\n" + logo);
+        System.out.println("Hello from\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("_______________________________________________________");
 
-        Scanner input = new Scanner (System.in);
-
-        while (true) {
-            String line = input.nextLine();
-
-            if (line.equalsIgnoreCase("bye")) {
-                System.out.println("_______________________________________________________");
-                System.out.println("Bye! See you again");
-                System.out.println("_______________________________________________________");
-                break;
-            }
-            else {
-                System.out.println("_______________________________________________________");
-                System.out.println(line);
-                System.out.println("_______________________________________________________");
-            }
-        }
+        System.out.println("Bye! Hope to see you again soon!");
     }
 }
